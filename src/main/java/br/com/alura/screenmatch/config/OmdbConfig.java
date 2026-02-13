1f5f142cbd3c3567c38e02cb33a7107ef731a85d
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-02-12 22:51:18 -0300

    chore: retirar atributo desnecessário de configuração

--- src/main/java/br/com/alura/screenmatch/config/OmdbConfig.java
+++ src/main/java/br/com/alura/screenmatch/config/OmdbConfig.java
@@ -1,22 +1,12 @@
 package br.com.alura.screenmatch.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "omdb")
 public class OmdbConfig {
     private String apikey;
-    private String url;
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
 
     public String getApikey() {
         return apikey;
